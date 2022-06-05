7191bc404c7e403daf71703ebf3024bb32ace9a6
Joyce Shu <jinr.shu@gmail.com>
2022-06-04 20:46:57 -0700

    MergeSort fixError

--- src/Sort/MergeSort.java
+++ src/Sort/MergeSort.java
@@ -13,7 +13,7 @@ public class MergeSort {
 
     private static void swap(Comparable[] a, int i, int j){
         Comparable tmp = a[i];
-        a[j] = a[j];
+        a[i] = a[j];
         a[j] = tmp;
     }
     /*
